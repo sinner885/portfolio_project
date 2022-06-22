17d93bcd4fe5e21585c78312fae1b434b7a85205
Mutabor <sinner885@gmail.com>
2022-06-22 19:04:13 +0300

    .gitignore

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.core.paginator import Paginator
+
 
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')[:5]
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+# def all_blogs(request):
+#     blogs = Blog.objects.order_by('-date')
+#     current_page = Paginator(blogs, 3)
+#     return render(request, 'blog/all_blogs.html', {'blogs': current_page})
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
