be235faef4e3ba77c618a856714f90598134be13
Mutabor <sinner885@gmail.com>
2022-06-23 10:16:23 +0300

    Добавлен djangop-ckeditor

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import Blog
 
-admin.site.register(Blog)
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+
+class BlogAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
+
+admin.site.register(Blog, BlogAdmin)
--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -23,6 +23,7 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('blog/', include('blog.urls')),
 ]
 
